3aa177dd629b4124676b479dd9ad89de1fc32652
strawloki <laurplayscevrea@gmail.com>
2022-12-05 14:10:12 +0100

    Added inputs for weight, resting pulse and max. pulse to the program.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace Kondital
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //save weight, rest pulse and max pulse
+            double weight, rPulse, mPulse;
+            bool infoInputted = false;
+            while(!infoInputted)
+            {
+
+                Console.WriteLine("Enter your weight below:");
+                string weightStr = Console.ReadLine();
+
+                if(double.TryParse(weightStr, out weight))
+                {
+                    Console.WriteLine("Enter your resting pulse below:");
+                    string pulseStr = Console.ReadLine();
+
+                    if(double.TryParse(pulseStr, out rPulse))
+                    {
+                        Console.WriteLine("Enter your max pulse below:");
+                        string maxPulseStr = Console.ReadLine();
+
+                        if(double.TryParse(maxPulseStr, out mPulse))
+                        {
+                            infoInputted = true;
+                            Console.WriteLine($"Success! {weight}, {rPulse}, {mPulse}");
+                        }
+                    }
+                }
+
+            }
+
         }
     }
 }
